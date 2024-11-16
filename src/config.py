0df557dd86da52e870430eb39723aec3819e0d6a
Tanvir Hossain <tanvirhossain.butex@gmail.com>
2024-11-16 12:39:24 +0600

    updated project configuration

--- src/config.py
+++ src/config.py
@@ -3,13 +3,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+# POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+POSTGRES_HOST = 'localhost'
 POSTGRES_PORT = os.getenv('POSTGRES_PORT')
 POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 POSTGRES_DB = os.getenv('POSTGRES_DB')
 
-DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 
